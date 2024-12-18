a63e01b966146d4ed6ad48af08a6e72aceeffbab
Riccardo Spadon <spadon.riccardo@gmail.com>
2024-12-18 15:54:48 +0100

    quiz-natalizio
    - Implementata la funzione "showQuestionsAndAnswers()" che mostra le domande e le risposte del quiz, prendendole dal file "questions.js";
    - Implementata la funzione "shuffleAnswers()" che "randomizza" l'ordine delle domande;

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -69,3 +69,85 @@ function nextQuestion() {
 
     timeout()
 }
+
+
+function showQuestionAndAnswer() {
+    const question = shuffledQuestions[index]
+
+    let answers = question.incorrect_answers
+    answers.push(question.correct_answer)
+    answers = shuffledAnswers(answers)
+
+    questionContainer.innerHTML = ''
+    answersContainer.innerHTML = ''
+
+    let h2 = document.createElement('h2')
+    h2.className = QUESTION_CLASS_NAME
+    h2.style.userSelect = 'none'
+    let p1 = document.createElement('p')
+    p1.style.margin = '0px'
+    let p2 = document.createElement('p')
+    p2.style.margin = '0px'
+
+    let currentQuestion = question.question
+    let arrayCharsQuestion = currentQuestion.split(' ')
+    let firstHalfQuestion = arrayCharsQuestion.slice(0, arrayCharsQuestion.length/2).join(' ')
+    let secondHalfQuestion = arrayCharsQuestion.slice(arrayCharsQuestion.length/2, arrayCharsQuestion.length).join(' ')
+    p1.innerHTML = firstHalfQuestion
+    p2.innerHTML = secondHalfQuestion
+
+    h2.appendChild(p1)
+    h2.appendChild(p2)
+    questionContainer.appendChild(h2)
+
+    for(let i = 0; i < answers.length; i++) {
+        let div = document.createElement('div')
+        div.className = 'item'
+
+        answersContainer.appendChild(div)
+
+        let input = document.createElement('input')
+        input.setAttribute('type', 'radio')
+        input.setAttribute('name', INPUT_ATTRIBUTE_NAME)
+        input.value =  answers[i]
+
+        currentInputButtons.push(input)
+
+        div.appendChild(input)
+
+        let label = document.createElement('label')
+        label.style.userSelect = 'none'
+        label.style.display = 'inline-block'
+        label.innerHTML = answers[i]
+        div.appendChild(label)
+
+        div.addEventListener('click', function answersClick(){
+            let children = this.childNodes
+            let input = children[0]
+            if(!input.checked){
+                input.checked = true
+            } else {
+                input.checked = false
+            }
+        })
+    }
+
+    let maxLabelLength = currentInputButtons[0].parentNode.childNodes[1].offsetWidth
+
+    for (let i = 1; i < currentInputButtons.length; i++) {
+        if(currentInputButtons[i].parentNode.childNodes[1].offsetWidth > maxLabelLength) {
+            maxLabelLength = currentInputButtons[i].parentNode.childNodes[1].offsetWidth
+        }
+    }
+
+    for (let i = 0; i < currentInputButtons.length; i++) {
+       let label = currentInputButtons[i].parentNode.childNodes[1]
+       label.style.minWidth = maxLabelLength + 'px'
+    }
+    index++
+}
+
+function shuffleAnswers(arr){
+    return arr.sort(() => Math.random() - 0.5)
+}
+
